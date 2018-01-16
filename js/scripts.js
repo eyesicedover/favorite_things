96b57c9f44f0c998d4928c8b1a6c77eba84c2085
Mariah Shipley and Stephanie Faber <mariah@>
2018-01-16 10:25:34 -0800

    add items pushed to the mixed array, add a list of the extended mixed array

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,15 @@ $(document).ready(function() {
     $("#output").text(favoritesArray);
     var mixedFavoritesArray = [favoritesArray[3], favoritesArray[0], favoritesArray[1], favoritesArray[4], favoritesArray[2]];
     $("#output2").text(mixedFavoritesArray);
+    mixedFavoritesArray.push("purple", "cat", "bananas");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[0] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[1] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[2] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[3] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[4] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[5] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[6] + "</li>");
+    $("ul#output3").append("<li>" + mixedFavoritesArray[7] + "</li>");
     event.preventDefault();
   });
 });
